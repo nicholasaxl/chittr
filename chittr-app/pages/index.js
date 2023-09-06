e71d889b3fec9cb6bfb881ad81abfa72314b5bf8
Nicholas Axl <a.nicholasaxl@gmail.com>
2023-09-06 10:52:36 +0700

    updated imports

--- chittr-app/pages/index.js
+++ chittr-app/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ChatBox from '../components/ChatBox';
+import chatBox from '../components/chatBox';
 
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-semibold mb-4">Chittr.AI</h1>
-      <ChatBox />
+      <chatBox />
     </div>
   );
 }
