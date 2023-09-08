d2e8bb80b89082eb47ea5a867a460db5f56bf82f
Nicholas Axl <a.nicholasaxl@gmail.com>
2023-09-08 19:09:36 +0700

    backend change to load youtube

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
-import { JSONLoader } from "langchain/document_loaders/fs/json";
+import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
 import express from 'express';
 import cors from 'cors';
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
-const loader = new JSONLoader(
-  "document_loaders/example_data/example.json",
-  ["/from", "/surname"]
-);
+//test with what is a programming language
+const loader = YoutubeLoader.createFromUrl("https://youtu.be/XASY30EfGAc?feature=shared", {
+  language: "en",
+  addVideoInfo: true,
+});
 
 const docs = await loader.load();
 
