27f366dd6289324f66d666687b205b79464ced9c
Nicholas Axl <a.nicholasaxl@gmail.com>
2023-09-13 13:32:23 +0700

    vercel again

--- chittr-app/components/chatBox.js
+++ chittr-app/components/chatBox.js
@@ -19,7 +19,7 @@ function ChatBox() {
   async function fetchBotResponse(q) {
 
     const res = await axios.post(
-      "http://localhost:3001", 
+      "https://chittr-8cbeekx92-nicholasaxl.vercel.app/", 
       {question: q},
       {headers: { "Content-Type": "application/json" }}
     );
