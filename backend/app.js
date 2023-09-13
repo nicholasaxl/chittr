ffc41b3d7eaf1793767bec5695405e512dc8f042
Nicholas Axl <a.nicholasaxl@gmail.com>
2023-09-13 12:32:31 +0700

    a

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+dotenv.config();
 
+import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
 import { PineconeClient } from "@pinecone-database/pinecone";
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
@@ -12,7 +15,10 @@ import { sleep } from 'openai/core.js';
 import bodyParser from 'body-parser';
 
 
-const loader = new TextLoader("document_loaders/example_data/cs188.txt");
+const loader = YoutubeLoader.createFromUrl("https://youtu.be/V-NH5orGUi8?si=gM38t5jAFVjzgF0h", {
+  language: "en",
+  addVideoInfo: true,
+});
 const docs = await loader.load();
 
 var question = "";
@@ -36,6 +42,12 @@ const app = express()
 const port = 3001
 app.use(cors(), bodyParser.json());
 
+//send
+// app.get('/', (req, res) => {
+//   res.send(docs)
+// })
+
+//query
 app.post('/', async (req, res) => {
   question = req.body.question;
   resultText = await queryPinecone(client, indexName, question);
