49bf900c1ae2e1342cf61c5a3a425df1e23002a3
Nicholas Axl <a.nicholasaxl@gmail.com>
2023-09-13 15:06:41 +0700

    ssssssssssss

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,60 @@
-// import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-// dotenv.config();
-//
+// // import dotenv from 'dotenv';
+// import express from 'express';
+// import cors from 'cors';
+// // dotenv.config();
+// //
 
-import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
-import { PineconeClient } from "@pinecone-database/pinecone";
-import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { createIndex } from "./createIndex.js";
-import { updatePinecone } from "./updatePinecone.js";
-import { queryPinecone } from "./queryPinecone.js";
-import { TextLoader } from "langchain/document_loaders/fs/text";
-import { sleep } from 'openai/core.js';
-import bodyParser from 'body-parser';
+// import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
+// import { PineconeClient } from "@pinecone-database/pinecone";
+// import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+// import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+// import { createIndex } from "./createIndex.js";
+// import { updatePinecone } from "./updatePinecone.js";
+// import { queryPinecone } from "./queryPinecone.js";
+// import { TextLoader } from "langchain/document_loaders/fs/text";
+// import { sleep } from 'openai/core.js';
+// import bodyParser from 'body-parser';
 
 
-// const loader = YoutubeLoader.createFromUrl("https://youtu.be/V-NH5orGUi8?si=gM38t5jAFVjzgF0h", {
-//   language: "en",
-//   addVideoInfo: true,
+// // const loader = YoutubeLoader.createFromUrl("https://youtu.be/V-NH5orGUi8?si=gM38t5jAFVjzgF0h", {
+// //   language: "en",
+// //   addVideoInfo: true,
+// // });
+// // const docs = await loader.load();
+
+// var question = "";
+// var resultText = "";
+
+// const indexName = "chittr-cs188-test";
+// const vectorDimension = 1536;
+// const client = new PineconeClient();
+// await client.init({
+//   apiKey: process.env.PINECONE_API_KEY,
+//   environment: "gcp-starter",
 // });
-// const docs = await loader.load();
 
-var question = "";
-var resultText = "";
+// (async () => {
+//     // await createIndex(client, indexName, vectorDimension);
+//     // await updatePinecone(client, indexName, docs);
+// })();
 
-const indexName = "chittr-cs188-test";
-const vectorDimension = 1536;
-const client = new PineconeClient();
-await client.init({
-  apiKey: process.env.PINECONE_API_KEY,
-  environment: "gcp-starter",
-});
+// //express 
+// const app = express()
+// const port = 3001
+// app.use(cors(), bodyParser.json());
 
-(async () => {
-    // await createIndex(client, indexName, vectorDimension);
-    // await updatePinecone(client, indexName, docs);
-})();
+// //send
+// // app.get('/', (req, res) => {
+// //   res.send(docs)
+// // })
 
-//express 
-const app = express()
-const port = 3001
-app.use(cors(), bodyParser.json());
-
-//send
-// app.get('/', (req, res) => {
-//   res.send(docs)
+// //query
+// app.post('/', async (req, res) => {
+//   question = req.body.question;
+//   resultText = await queryPinecone(client, indexName, question);
+//   res.send(resultText)
 // })
 
-//query
-app.post('/', async (req, res) => {
-  question = req.body.question;
-  resultText = await queryPinecone(client, indexName, question);
-  res.send(resultText)
-})
-
-app.listen(port, () => {
-  console.log("listening on port 3001")
-})
+// app.listen(port, () => {
+//   console.log("listening on port 3001")
+// })
